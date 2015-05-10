2284ba2d192173cec92655f324d5c5576343036c
E9hGxKCf <pspsospspsos@yahoo.co.jp>
2015-05-11 02:11:29 +0900

    コミットテスト

--- main.cpp
+++ main.cpp
@@ -2,21 +2,27 @@
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	//画面モードをウィンドウに変更
 	ChangeWindowMode(true);
+
+	//描画先を裏画面に変更
 	SetDrawScreen(DX_SCREEN_BACK);
 
+	//Dxライブラリの初期化
 	if (DxLib_Init() == -1)
 		return -1;
 
+	//ゲームループ
 	while (!ProcessMessage())
 	{
+		//画面初期化
 		ClearDrawScreen();
 
-		DrawPixel(320, 240, 0xffff);
-
+		//裏画面情報を表画面に転送
 		ScreenFlip();
 	}
 
+	//Dxライブラリの終了処理
 	DxLib_End();
 
 	return 0;
